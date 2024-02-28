48bd69e4e01f2efe7b14819b4772caa04b1c0e7f
unknown <harinarayana50807@gmail.com>
2024-02-28 12:02:12 +0530

    added

--- travelproject/travelproject/credentials/views.py
+++ travelproject/travelproject/credentials/views.py
@@ -3,21 +3,24 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 
+from django.contrib import auth, messages
+from django.shortcuts import render, redirect
+
 def login(request):
-    if request.method=='POST':
-        username=request.POST['Username']
-        password=request.POST['Password']
-        user=auth.authenticate(username=username,password=password)
+    if request.method == 'POST':
+        username = request.POST.get('Username')
+        password = request.POST.get('Password')
+        user = auth.authenticate(username=username, password=password)
 
         if user is not None:
-            auth.login(request,user)
+            auth.login(request, user)
             return redirect('/')
         else:
-            messages.info(request,'invalid credentails')
-            return redirect('login')
-    return render(request,'login.html')
-    
+            messages.error(request, 'Invalid credentials')
+            return render(request, 'login.html', {'invalid_credentials': True})
+    return render(request, 'login.html')
 
+    
 
 def register(request):
     if request.method=='POST':
